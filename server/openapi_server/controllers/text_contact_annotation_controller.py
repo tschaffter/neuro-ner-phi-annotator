c9adbdc93031d9362fde4185fee834e1cda5ebb2
Clement <clement.essien@sagebase.org>
2021-07-29 14:06:06 -0500

    added neuroner model to contact and location controller

--- server/openapi_server/controllers/text_contact_annotation_controller.py
+++ server/openapi_server/controllers/text_contact_annotation_controller.py
@@ -4,7 +4,7 @@ from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.text_contact_annotation_request import TextContactAnnotationRequest  # noqa: E501
 from openapi_server.models.text_contact_annotation import TextContactAnnotation
 from openapi_server.models.text_contact_annotation_response import TextContactAnnotationResponse  # noqa: E501
-
+from openapi_server import neuro_model as model
 
 def create_text_contact_annotations(text_contact_annotation_request=None):  # noqa: E501
     """Annotate contacts in a clinical note
@@ -19,17 +19,10 @@ def create_text_contact_annotations(text_contact_annotation_request=None):  # no
             note = annotation_request._note
             print(note)
             annotations = []
-            matches = re.finditer(r"\([\d]{3}\)\s[\d]{3}-[\d]{4}", note._text)
-            add_contact_annotation(annotations, matches, "phone")
-
-            matches = re.finditer(r"[\d]{3}-[\d]{3}-[\d]{4}", note._text)
-            add_contact_annotation(annotations, matches, "phone")
-
-            matches = re.finditer(r"[\S]+@[\S]+", note._text)
-            add_contact_annotation(annotations, matches, "email")
-
-            matches = re.finditer(r"https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}", note._text)  # noqa: E501
-            add_contact_annotation(annotations, matches, "url")
+            print(note._text)
+            matches = model.predict(note._text)
+            
+            add_contact_annotation(annotations, matches)
             res = TextContactAnnotationResponse(annotations)
             status = 200
         except Exception as error:
@@ -38,16 +31,17 @@ def create_text_contact_annotations(text_contact_annotation_request=None):  # no
     return res, status
 
 
-def add_contact_annotation(annotations, matches, contact_type):
+def add_contact_annotation(annotations, matches):
     """
     Converts matches to TextContactAnnotation objects and adds them to the
     annotations array specified.
     """
     for match in matches:
-        annotations.append(TextContactAnnotation(
-            start=match.start(),
-            length=len(match[0]),
-            text=match[0],
-            contact_type=contact_type,
-            confidence=95.5
-        ))
+        if match['type'] in ["PHONE"]:
+            annotations.append(TextContactAnnotation(
+                start=match['start'],
+                length=len(match['text']),
+                text=match['text'],
+                contact_type=match['type'],
+                confidence=95.5
+            ))
