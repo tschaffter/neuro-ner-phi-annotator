d0a46e221a37ccd780485ba3e490a05a2d52c862
Clement <clement.essien@sagebase.org>
2021-07-30 07:22:12 -0500

    dockerized the neuro-ner tool

--- server/openapi_server/neuro_model.py
+++ server/openapi_server/neuro_model.py
@@ -1,6 +1,4 @@
-
 from neuroner import neuromodel
-import os
 
 class Neuroner:
     def __init__(self):
@@ -9,7 +7,7 @@ class Neuroner:
         pretrained_model_folder ="openapi_server/trained_models/i2b2_2014_glove_spacy_bioes", 
         parameters_filepath='.', spacylanguage="en_core_web_sm", 
         token_pretrained_embedding_filepath='openapi_server/data/word_vectors/glove.6B.100d.txt', 
-        load_all_pretrained_token_embeddings=1,maximum_number_of_epochs=100)
+        load_all_pretrained_token_embeddings=1,maximum_number_of_epochs=500)
         
 
 model= Neuroner().nn
